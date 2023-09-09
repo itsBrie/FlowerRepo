81ec4c2b563c85311e274bc790032ff493432b79
Brie <briecrafty@gmail.com>
2023-09-08 22:38:59 -0500

    Initial Commit

---
+++ flower-dataset/src/main/java/com/brie/flower/FlowerDatasetApplication.java
@@ -0,0 +1,13 @@
+package com.brie.flower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerDatasetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerDatasetApplication.class, args);
+	}
+
+}
---
+++ flower-dataset/src/test/java/com/brie/flower/FlowerDatasetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brie.flower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerDatasetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
